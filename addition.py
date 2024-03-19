d9252516ce1cd676c7bd3e24ce4d29eb3eded3d6
Prithvi Rachakonda <rachakondalohit@gmail.com>
2024-03-19 13:48:48 -0400

    Added addition file.

---
+++ addition.py
@@ -0,0 +1,12 @@
+def addition(a, b):
+    '''This method will help add 2 integers received from user as input'''
+    c = a + b
+    return c
+
+def main():
+    a = int(input('Enter the first number: '))
+    b = int(input('Enter the second number: '))
+    print('The sum of the two numbers is: ', addition(a, b))
+
+if __name__ == '__main__':
+    main()
